295711688c7391819a39f53463f57854dd6f0116
Abhishek Gupta <51535996+abhi4418@users.noreply.github.com>
2024-03-01 22:54:08 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -47,7 +47,7 @@ const server = app.listen(PORT , console.log(`Started on port PORT ${PORT}`)) ;
 const io = require('socket.io')(server , {
     pingTimeout : 60000,
     cors : {
-        origins: ["*"]
+        origin: "*"
     }
 }) ;
 
